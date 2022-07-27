99092036958d10d776c9bf9a4561a3e5401feb6f
FQX117 <blackfox950@gmail.com>
2022-07-27 18:56:25 -0500

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route("/status", methods=['GET'], stict_slashes=False)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
 	'''show the status function'''
 	return jsonify({"status": "OK"})
