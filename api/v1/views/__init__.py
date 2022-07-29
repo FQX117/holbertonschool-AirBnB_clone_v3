efb7f37cf3ec89b43f19e6bf4578f1a01d326a85
FQX117 <blackfox950@gmail.com>
2022-07-29 13:53:18 -0500

    fix 4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """blue print veiws"""
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from flask import Blueprint 
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.states import *
