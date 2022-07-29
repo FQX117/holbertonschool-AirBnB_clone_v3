5834074d83cfa2f6b8afcfa562eb1c974484082c
FQX117 <blackfox950@gmail.com>
2022-07-28 20:41:02 -0500

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,14 +8,3 @@ app_views = Blueprint("app_views",__name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
-=======
-#!/usr/bin/python3
-'''blue print veiws '''
-from flask import Blueprint 
-
-
-app_views = Blueprint("app_views",__name__, url_prefix="/api/v1")
-
-from api.v1.views.index import *
-from api.v1.views.users import *
->>>>>>> 574b4636c9659ce8896286ca33d5f5bf5c12ec3f
