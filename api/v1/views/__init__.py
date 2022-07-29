4f01d874389f2d2d0565d06af5980e01754e6fbb
FQX117 <blackfox950@gmail.com>
2022-07-29 13:51:57 -0500

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """blue print veiws"""
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from flask import Blueprint 
 from api.v1.views.index import *
 from api.v1.views.users import *
@@ -7,5 +8,3 @@ from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
