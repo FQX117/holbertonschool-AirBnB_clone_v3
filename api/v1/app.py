9a7889303356c0114e632a881a2108a7d0d8c4c9
FQX117 <blackfox950@gmail.com>
2022-07-28 21:46:48 -0500

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """return status of API"""
-from flask import Flask, jsonify, Blueprint
+
+from flask import Flask, jsonify
 from models import storage
-from flask_cors import CORS
-from api.v1.views import app_views 
+from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,20 +16,17 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
-	'''exit storage'''
-	storage.close()
+    '''exit storage'''
+    storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     """json 404"""
-    return jsonify({"error": "Not found"}), 404
+    return (jsonify({"error": "Not found"}), 404)
 
-if __name__ == '__main__':
-    host = "0.0.0.0"
-    port = '5000'
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == "__main__":
+    hosts = getenv('HBNB_API_HOST', default='0.0.0.0')
+    ports = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=hosts, port=ports, threaded=True)
